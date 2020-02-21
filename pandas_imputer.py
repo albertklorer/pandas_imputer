ba861f2aed6713ab71970a0d923877437da3bb22
Albert Klorer <albertklorer@mwc-033040.dhcp.missouri.edu>
2020-02-21 11:14:09 -0600

    function declerations

--- pandas_imputer.py
+++ pandas_imputer.py
@@ -1,2 +1,17 @@
 import pandas as pd
 
+"""
+    Impute na values of dataframe.
+    df: Input dataframe to impute.
+    columns: List of columns to apply imputation to. Given as list of strings.
+    data_types: Data types of given columns. Given as list of strings. Use
+        'numerical' for numerical data and 'categorical' for categorical data.
+    method: Method used for imputation. Given as a string. Use 'k_nearest_neighbors'
+        for k nearest neighbors and 'decision_tree' for decision tree, and 
+        'random_forest' for random forest.
+"""
+def impute_na(df, columns, data_types, method='decision_tree') -> pd.DataFrame:
+    print(df)
+
+iris = pd.read_csv('iris.csv', index_col=False)
+print(iris.head)
